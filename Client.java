368aac16694866d098f2e45a77bb1c624150bd71
MILIND SINGH <milluk813@gmail.com>
2024-12-05 14:17:17 +0530

    real time feature added

--- Client.java
+++ Client.java
@@ -10,22 +10,28 @@ import javax.swing.*;
 import javax.swing.border.*;
 import java.util.*;
 import java.text.*;
+import java.net.*;
+import java.io.*;
 
-public class Client extends JFrame implements ActionListener{
+
+public class Client implements ActionListener{
 
     JTextField text;
-    JPanel a1;
-    Box vertical = Box.createVerticalBox();
+    static JPanel a1;
+    static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+
+    static DataOutputStream dout;
 
     Client(){
 
-        setLayout(null);
+        f.setLayout(null);
 
         JPanel p1 = new JPanel();
         p1.setBackground(new Color(7,94,84));
         p1.setBounds(0,0,450,70);
         p1.setLayout(null);
-        add(p1);
+        f.add(p1);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
@@ -85,14 +91,14 @@ public class Client extends JFrame implements ActionListener{
 
         a1 = new JPanel();
         a1.setBounds(5,75,440,570);
-        add(a1);
+        f.add(a1);
 
         
 
         text = new JTextField();
         text.setBounds(5, 655, 310, 40);
         text.setFont(new Font("SAN_SERIF", Font.BOLD, 16));
-        add(text);
+        f.add(text);
 
         JButton send = new JButton("Send");
         send.setBounds(320, 655, 123, 40);
@@ -100,35 +106,39 @@ public class Client extends JFrame implements ActionListener{
         send.setForeground(Color.WHITE);
         send.addActionListener(this);
         send.setFont(new Font("SAN_SERIF", Font.BOLD, 16));
-        add(send);
+        f.add(send);
 
-
-        setSize(450, 700);   
-        setLocation(800,50);
-        setUndecorated(true);
-        getContentPane().setBackground(Color.WHITE);
-        setVisible(true);
+        f.setSize(450, 700);   
+        f.setLocation(800,50);
+        f.setUndecorated(true);
+        f.getContentPane().setBackground(Color.WHITE);
+        f.setVisible(true);
 
     }
     public void actionPerformed(ActionEvent ae){
-        String out = text.getText();
+        try{
+            String out = text.getText();
 
-        JPanel p2 = formatLabel(out);
+            JPanel p2 = formatLabel(out);
 
-        a1.setLayout(new BorderLayout());
+            a1.setLayout(new BorderLayout());
 
-        JPanel right = new JPanel(new BorderLayout());
-        right.add(p2, BorderLayout.LINE_END);
-        vertical.add(right);
-        vertical.add(Box.createVerticalStrut(15));
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
 
-        a1.add(vertical, BorderLayout.PAGE_START);
+            a1.add(vertical, BorderLayout.PAGE_START);
+            dout.writeUTF(out);
 
-        text.setText("");
+            text.setText("");
 
-        repaint();
-        invalidate();
-        validate();
+            f.repaint();
+            f.invalidate();
+            f.validate();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
@@ -157,9 +167,56 @@ public class Client extends JFrame implements ActionListener{
 
     public static void main(String[] args){
         new Client();
+
+        try{
+            Socket s = new Socket("127.0.0.1", 6001);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            dout = new DataOutputStream(s.getOutputStream());
+
+            while(true) {
+                a1.setLayout(new BorderLayout());
+                String msg = din.readUTF();
+                JPanel panel = formatLabel(msg);
+
+                JPanel left = new JPanel(new BorderLayout());
+                left.add(panel, BorderLayout.LINE_START);
+                vertical.add(left);
+                vertical.add(Box.createVerticalStrut(15));
+                a1.add(vertical, BorderLayout.PAGE_START);
+
+               
+                f.validate();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
